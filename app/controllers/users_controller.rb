1f8f9895e0cc7b3adb1e3cc6194f09c2a0276f90
Murad Sarker <sarker24@gmail.com>
2020-10-15 01:11:06 +0200

    Redirect  login and show the list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def show
-    @user = User.find(params[:id])
+   
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
   
@@ -13,11 +14,11 @@ class UsersController < ApplicationController
     end
     
     def edit
-      @user = User.find(params[:id])
+      
     end
    
     def update
-      @user = User.find(params[:id])
+      
 
       if @user.update(user_params)
         flash[:notice] = "Your account information updates successfully"
@@ -31,6 +32,7 @@ class UsersController < ApplicationController
     def create
        @user = User.new(user_params)
        if @user.save
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to the alpha #{@user.username} blog"
         redirect_to articles_path
        else
@@ -41,5 +43,9 @@ class UsersController < ApplicationController
     private 
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
     end
 end
